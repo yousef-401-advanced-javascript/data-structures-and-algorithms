f5d95702966f2d563f3b6bdfade480682be67d84
yousef-97 <alshenyousef@gmail.com>
2020-05-31 23:50:07 +0300

    Done

--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -12,6 +12,7 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+  ////////////////insert at the beginning\\\\\\\\\\\\
   insert(theNewValue) {
     if (!theNewValue){
       try {
@@ -32,10 +33,9 @@ class LinkedList {
     node.next = this.head;
     this.head = node;
     currentNode = this.head;
-    console.log(this);
     return this;
   }
-
+  ////////////////////////include or not\\\\\\\\\\\\\\\\
   includes(searchingValue) {
     let currentNode = this.head;
     while (currentNode){
@@ -46,6 +46,7 @@ class LinkedList {
     }
     return false;
   }
+  ///////////////////////convert the linked list to string\\\\\\\\\\\\\\\\\\\\
   toString(){
     let currentNode = this.head;
     let str = '';
@@ -56,22 +57,97 @@ class LinkedList {
     str += 'NULL';
     return str;
   }
+  ///////////////////////append at the end\\\\\\\\\\\\\\\\\\\\\
+  append(value){
+    let node = new Node(value);
+    if(!this.head){
+      this.head = node;
+      return this;
+    }
+    let currentNode = this.head;
+    while(currentNode.next){
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+
+  }
+  /////////////////////insert before\\\\\\\\\\\\\\\\\\\
+  insertBefore(beforeValue, newVal){
+    let node = new Node(newVal);
+    if(!this.head){
+      return(`the linked list is empty` );
+    }
+    let current = this.head;
+    while(current.next !== null){
+      if(current.next.value === beforeValue){
+        node.next = current.next;
+        current.next = node;
+        return this;
+      }
+      current = current.next;
+    }
+    return (`this value ${beforeValue} is not exest in the linked list` );
+
+  }
+  ////////////////insert after\\\\\\\\\\\\\\\\\
+  insertAfter(afterValue, newVal){
+    let node = new Node(newVal);
+    if(!this.head){
+      return (`the linked list is empty` );
+      
+    }
+    let current = this.head;
+    while(current.next !== null){
+      if(current.value === afterValue){
+        node.next = current.next;
+        current.next = node;
+     
+        return this;
+      }
+      current = current.next;
+    }
+    return (`this value ${afterValue} is not exest in the linked list` );
+  }
 }
 module.exports = new LinkedList;
 
 let hi = new LinkedList();
 // console.log(hi.includes(1));
-
+////////////////////////insert\\\\\\\\\\\\\\\\\
 // hi.insert(1222);
 // hi.insert();
 // hi.insert(3);
 // hi.insert(4);
 // hi.insert(5);
+// console.log(hi.toString());
+
+//////////////////include\\\\\\\\\\\\\\\\\\\\
 // console.log(hi.includes(1));
 // console.log(hi.includes(10));
 // console.log(hi.includes(14));
 // console.log(hi.includes(4));
+
+////////////////////appending\\\\\\\\\\\\\\
+// hi.append(3);
+// hi.append(4);
+// hi.append(5);
+// hi.append(12);
+// hi.append(5);
+// hi.append(11);
+// hi.append(5);
+// hi.append(14);
 // console.log(hi.toString());
+// hi.insertBefore(5,99);
+// hi.insertAfter(111,190);
+// hi.insertBefore(14);
+// hi.insertBefore(14);
+// console.log(hi.toString());
+
+
+
+
+
 
 
 
--- challenges/linkedList/linked-list.test.js
+++ challenges/linkedList/linked-list.test.js
@@ -1,5 +1,6 @@
 'use strict';
 const LinkedList = require('./linked-list.js');
+
 describe('Linked List insert(number)',()=>{
   test ('add anumber when the linked list is empty',()=>{
     // console.log(LinkedList.insert(1).head);
@@ -17,5 +18,32 @@ describe('Linked List insert(number)',()=>{
   });
   test('to string ',()=>{
     expect(LinkedList.toString()).toEqual('{2} -> {1} -> NULL');
+  });
+});
+describe('insertBefore, insertAfter, append  methods',()=>{
+  test ('appending while the linked list is empty',()=>{
+    expect(LinkedList.append(10).head).toEqual(LinkedList.head);
+  });
+  test ('add anumber at the end of the linked list',()=>{
+    // console.log(LinkedList.insert(1).head);
+    expect(LinkedList.append(2).head).toEqual(LinkedList.head);
+  });
+  test('insetrbefor if the the node is not exist',()=>{
+    let error = `this value 15 is not exest in the linked list`;
+    expect(LinkedList.insertBefore(15, 10)).toMatch(error);
+  });
+  test('insetrbefor 1',()=>{
+    expect(LinkedList.insertBefore(1, 30).head).toEqual(LinkedList.head);
+  });
+  test('insetrAfter if the the node is not exist',()=>{
+    let error = `this value 15 is not exest in the linked list`;
+    expect(LinkedList.insertBefore(15, 10)).toMatch(error);
+  });
+  test('insetrAfter 1',()=>{
+    expect(LinkedList.insertAfter(1, 100).head).toEqual(LinkedList.head);
+  });
+
+  test('to string ',()=>{
+    expect(LinkedList.toString()).toEqual('{2} -> {30} -> {1} -> {100} -> {10} -> {2} -> NULL');
   });
 });
