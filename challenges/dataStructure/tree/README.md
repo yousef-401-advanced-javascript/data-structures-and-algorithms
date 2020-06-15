768e7d40fc9591ecc7720022c249a46001495a69
yousef-97 <alshenyousef@gmail.com>
2020-06-16 01:02:30 +0300

    the same of the reading tree but not working

--- challenges/dataStructure/tree/README.md
+++ challenges/dataStructure/tree/README.md
@@ -1,4 +1,4 @@
-# Stacks and Queues
+# TREE
 <!-- Short summary or background information -->
 ## Challenge
 <!-- Description of the challenge -->
@@ -8,14 +8,19 @@
 
 - Create a BinarySearchTree class Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree. Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
 
+- create method to the binary tree class called breadthFirst to return an array with the tree nodes values .
+
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
 - the BIG O for the method (add, contains) is O(logn)in time and O(1)for the space
+- the BIG O for the method (Breadth first) is O(n)in time and O(1)for the space
 
 ## Solution
 ![whiteboard images](../../../assets/binarySearchTree/BST1.jpg)
 ![whiteboard images](../../../assets/binarySearchTree/BST2.jpg)
 ![whiteboard images](../../../assets/binarySearchTree/BST3.jpg)
 ![whiteboard images](../../../assets/binarySearchTree/BST4.jpg)
+![whiteboard images](../../../assets/binarySearchTree/BF1.jpg)
+![whiteboard images](../../../assets/binarySearchTree/BF2.jpg)
 
 
--- challenges/dataStructure/tree/tree.js
+++ challenges/dataStructure/tree/tree.js
@@ -1,4 +1,49 @@
 'use strict';
+class Queue{
+  constructor(){
+    this.storage = null;
+    this.rare = null;
+    this.front = null;
+  }
+  enqueue(newValue){
+    let node = new Node(newValue);
+    if (!(this.front)){
+      this.front = node;
+      this.storage = node;
+      this.rare = node;
+      return this;
+    }
+    // node.next = this.storage;
+    // this.storage = node;
+    this.rare.next = node;
+    this.rare = node;
+
+    return this;
+    
+  }
+  dequeue(){
+    if(!this.front){
+      return 'Exception';
+    }
+    let temp = this.front;
+    this.front = this.front.next;
+    temp.next = null;
+    if(!this.front){
+      this.rare = null;
+    }
+    this.storage = this.front;//some thing weird
+    return temp.value;
+  }
+  peek(){
+    return this.front&&this.front.value||'Exception';
+  }
+  isEmpty(){
+    return !this.front;
+  }
+
+
+}
+///////////////////---------------TREES------------\\\\\\\\\\\\\\\\\\\\
 
 class Node {
   constructor(value, left = null, right = null) {
@@ -41,8 +86,32 @@ class BinaryTree {
     _traversal(this.root);
     return results;
   }
-}
+  breadthFirst() {///its the same psodeu code in  the reading but its not work 
+    const finalResults = [];
+    const breadth = new Queue();
+    breadth.enqueue(this.root);
+    console.log(breadth.front);
+    while (breadth.peek()) {
+      // console.log(breadth.peek());
+      // console.log(breadth);
 
+      const front = breadth.dequeue();
+      finalResults.push(front.val);
+
+      if (front.left) {
+        breadth.enqueue(front.left);
+      }
+
+      if (front.right) {
+        breadth.enqueue(front.right);
+      }
+    }
+
+    return finalResults;
+  }
+
+}
+///////////////////-------------Binary search tree---------------\\\\\\\\\\\\\\\\\\\\\\\\\
 class BinarySearchTree{
   constructor(root=null){
     this.root=root;
@@ -100,7 +169,25 @@ module.exports ={
   BST:BinarySearchTree,
   BT:BinaryTree,
 };
-
+const one = new Node(15);
+const two = new Node(3);
+const three = new Node(6);
+const four = new Node('dsd');
+const five = new Node(5);
+const six = new Node(75);
+const seven = new Node(12);
+const eight = new Node(25);
+const nine = new Node(9);
+one.left = two;
+one.right = three;
+three.right = four;
+three.left = five;
+two.left = six;
+six.right = seven;
+seven.left = eight;
+seven.right = nine;
+let tree = new BinaryTree(one);
+console.log(tree.breadthFirst());
 // let tree = new BinarySearchTree();
 // tree.add(10);
 // tree.add(12);
